4537c6d5b6f9a966adb6986cfaa1f54dee9583d5
lianghh <lianghh@lianghhdeMacBook-Pro.local>
2016-03-31 23:44:27 +0800

    add mysql sys info

---
+++ record_mysql_sys_info.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+INTERVAL=5
+PREFIX=$INTERVAL-sec-status
+RUNFILE=/Users/lianghh/lianghh_workspace/pros/mysql_sysbench/running
+
+mysql -h127.0.0.1 -uroot -e 'SHOW GLOBAL VARIABLES' >> mysql-variables
+
+while test -e $RUNFILE; do
+    file=$(date +%F_%I)
+    sleep=$(date +%s.%N | awk "{print $INTERVAL - (\$1 % $INTERVAL)}")
+    sleep $sleep
+    ts="$(date +"TS %s.%N %F %T")"
+    loadavg="$(uptime)"
+    echo "$ts $loadavg" >> $PREFIX-${file}-status
+    mysql -h127.0.0.1 -uroot -e 'SHOW GLOBAL STATUS' >> $PREFIX-${file}-status &
+    echo "$ts $loadavg" >> $PREFIX-${file}-innodbstatus
+    mysql -h127.0.0.1 -uroot -e 'SHOW ENGINE INNODB STATUS\G' >> $PREFIX-${file}-innodbstatus &
+    echo "$ts $loadavg" >> $PREFIX-${file}-processlist
+    mysql -h127.0.0.1 -uroot -e 'SHOW FULL PROCESSLIST\G' >> $PREFIX-${file}-processlist &
+    echo $ts
+done
+echo Exiting because $RUNFILE does not exist.
